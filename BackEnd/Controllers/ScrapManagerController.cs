213f249fae45b16094e4905ab24af5415b9c98a3
shujaat.siddiqui <shujaat.siddiqui@10pearls.local>
2019-12-10 19:07:53 +0500

    test

--- BackEnd/Controllers/ScrapManagerController.cs
+++ BackEnd/Controllers/ScrapManagerController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.Identity;
 using WebApi2.Data;
+using System.Linq;
 
 namespace WebApi2.Controllers
 {
@@ -35,10 +36,21 @@ namespace WebApi2.Controllers
         }
 
         [HttpPost]
-        [Authorize]
-        public async void Create()
+        [Authorize(Policy="RequireCustomerRole")]
+        public async void Create(List<Clientorders> obj)
         {
             var user = await _userManager.GetUserAsync(HttpContext.User);
+            obj.ForEach(x=>x.User = user);
+            _dbContext.Clientorders.AddRange(obj);
+            _dbContext.SaveChanges();            
+        }
+
+        [HttpGet]
+        [Authorize(Policy="RequireCustomerRole")]
+        public async Task<List<Clientorders>> GetOrdersList()
+        {
+            var user =  await _userManager.GetUserAsync(HttpContext.User);
+            return _dbContext.Clientorders.Where(x=>x.User == user).Select(x=>x).ToList();
         }
     }
 }
